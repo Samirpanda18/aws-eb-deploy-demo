5680911951720624b790f306004cdf53f7960bd0
samirpanda <samir.panda@nagarro.com>
2025-12-22 11:23:47 +0530

    test

--- src/main/java/com/example/demo/HealthController.java
+++ src/main/java/com/example/demo/HealthController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
-    @GetMapping("/")
-    public String home() {
+    @GetMapping("/healthz")
+    public String healthz() {
         return "OK";
     }
 }
